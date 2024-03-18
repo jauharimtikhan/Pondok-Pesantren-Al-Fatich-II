efcd29edd181177a25ebfaabb7e5e8c9a3660066
jauhar-imtikhan <bangjoe.muns@gmail.com>
2024-03-19 03:43:12 +0700

    fix larger content

--- Modules/Admin/resources/views/layouts/app.blade.php
+++ Modules/Admin/resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Al Fatih Admin Panel</title>
-    {{-- <link rel="shortcut icon" href="{{ asset('assets') }}/compiled/svg/favicon.svg" type="image/x-icon"> --}}
     <link rel="shortcut icon" href="{{ asset('assets-landing-page/img/favicon.ico') }}" type="image/png">
     <link rel="stylesheet" href="{{ asset('assets') }}/extensions/flatpickr/flatpickr.min.css">
     <link rel="stylesheet" href="{{ asset('assets') }}/compiled/css/app.css">
